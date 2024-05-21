7e9e1b29f4df5a5c65ec4e287b6401c77b01dd9b
Cyril Plisko <v-cyril.plisko@wallaroo.ai>
2024-05-21 13:18:22 +0300

    Build image (#9)
    * Add justfile
    * Build image

--- sccache.mk
+++ sccache.mk
@@ -6,7 +6,7 @@ DOCKER_SCCACHE := --build-context conductor=$(CONDUCTOR_LOCATION)
 ifdef CI
 GCS_CREDS ?= /run/secrets/gcs.json
 DOCKER_SCCACHE := $(DOCKER_SCCACHE) \
-		--build-arg GCS_CREDS=$(GCS_CREDS) \
+	--build-arg GCS_CREDS=$(GCS_CREDS) \
     --secret id=gcs,src=$(GOOGLE_APPLICATION_CREDENTIALS)
 else
 GCS_CREDS ?= ""
